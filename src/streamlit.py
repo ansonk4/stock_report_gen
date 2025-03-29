59139cdb6f5f65cae41cff7804da891d81ef0ba3
ansonk4 <kwoktsunon@yahoo.com>
2025-03-30 00:29:10 +0800

    update

--- src/streamlit.py
+++ src/streamlit.py
@@ -4,8 +4,9 @@ from report_generate import Config, ReportGenerator
 import os
 
 def main():
-    os.environ["OPENAI_API_KEY"] == st.secrets["OPENAI_API_KEY"]
-    os.environ["NEWSAPI_KEY"] == st.secrets["NEWSAPI_KEY"]
+    st.write(st.secrets["OPENAI_API_KEY"])
+    # os.environ["OPENAI_API_KEY"] == st.secrets["OPENAI_API_KEY"]
+    # os.environ["NEWSAPI_KEY"] == st.secrets["NEWSAPI_KEY"]
 
     st.title("Stock News Report Generator")
     
